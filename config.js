40cf745c171dabe3a49d1f366fa057923a1e2c3e
MarvellousOseh <140559292+MarvellousOseh@users.noreply.github.com>
2023-07-30 16:06:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -22,8 +22,8 @@ global.lApiKey = {// if this API key get expired then please go to https://api.l
 }
 
 
-global.botname = "Queen Anya" 
-global.author = "Enter Author Name Here"  
+global.botname = "Movie Hub Bot" 
+global.author = "Marvellous"  
 global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
 global.myweb = "Enter Your Any Website Profile Link Here"
 global.footer = "©\t" + "Enter Your Watermark Here"
@@ -34,15 +34,15 @@ global.socialText = `*⪧⪢ Please join this group for by bot's announcement.*`
 global.socialLink = `*⪧⪢ https://chat.whatsapp.com/HshHS6kHF9NHnA9lfMwwSM 💝*`
 
 
-global.ownername = "Enter Owner Name Here"    
-global.ownernumber = ["918811074852"]
-global.ytname = "YT: Enter Your Channel Name Here"
-global.continent = "Asia" // your continent name 
-global.region = "Enter Your Country Name Here"
-global.state = "Enter Your State Name Here" // your state name
-global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
+global.ownername = "Marvellous"    
+global.ownernumber = ["2348080965933"]
+global.ytname = "YT: Lonewolfmoviehub"
+global.continent = "Africa" // your continent name 
+global.region = "Nigeria"
+global.state = "Edo" // your state name
+global.timezone = "Africa/WAT" // search on google if you don't know the timezone of your country //
 global.instagramId = "Enter Your Insta ID Here"
-global.email = "Enter Your Email ID Here"
+global.email = "marvellousoseh3@gmail.com"
 
 
 // Greeting messages
